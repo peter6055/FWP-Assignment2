f0c1cd51edc48dfc70857616e62001623ba51d65
frocen <s3738959@student.rmit.edu.au>
2022-08-19 23:55:57 +1000

    new

--- a1/src/data/repository.js
+++ a1/src/data/repository.js
@@ -1,17 +1,19 @@
 const USERS_KEY = "users";
 const USER_KEY = "user";
+const POST_DATABASE = "post";
 
 // Initialise local storage "users" with data, if the data is already set this function returns immediately.
 function initUsers() {
     // Stop if data is already initialised.
-    if (localStorage.getItem(USERS_KEY) !== null)
-        return;
+    if (localStorage.getItem(USERS_KEY) === null){
+        const users = [];
+        localStorage.setItem(USERS_KEY, JSON.stringify(users));
+    }
 
-    // User data is hard-coded, passwords are in plain-text.
-    const users = [];
-
-    // Set data into local storage.
-    localStorage.setItem(USERS_KEY, JSON.stringify(users));
+    if (localStorage.getItem(POST_DATABASE) === null){
+        const post = [];
+        localStorage.setItem(POST_DATABASE, JSON.stringify(post));
+    }
 }
 
 function createUsers(username, password, email) {
@@ -30,11 +32,23 @@ function createUsers(username, password, email) {
             password: password,
             email: email,
             JoinDate: JoinDate,
-            id: id
+            id: id,
+        };
+
+    const post =
+        {
+            id : id,
+            post_data:[],
+            reply_data:[]
         };
     const users = getUsers();
     users.push(user);
+    const posts = getPosts();
+    console.log(posts)
+    posts.push(post);
+    console.log(posts)
     localStorage.setItem(USERS_KEY, JSON.stringify(users));
+    localStorage.setItem(POST_DATABASE, JSON.stringify(posts));
     localStorage.setItem(USER_KEY, JSON.stringify(id));
     return id;
 }
@@ -54,6 +68,11 @@ function getUsers() {
     const data = localStorage.getItem(USERS_KEY);
 
     // Convert data to objects.
+    return JSON.parse(data);
+}
+
+function getPosts(){
+    const data = localStorage.getItem(POST_DATABASE);
     return JSON.parse(data);
 }
 
@@ -118,6 +137,7 @@ function removeUser() {
 function changeName(id, newUsername) {
     const newUsers = [];
     const users = getUsers();
+    if (newUsername!==""){
         for (const user of users) {
             if (id === user.id) {
                 user.username = newUsername;
@@ -126,7 +146,9 @@ function changeName(id, newUsername) {
         }
         localStorage.setItem(USERS_KEY, JSON.stringify(newUsers));
         return true;
-   
+    }else {
+        alert("Name can not be empty")
+    }
 }
 
 function changeEmail(id, newEmail) {
