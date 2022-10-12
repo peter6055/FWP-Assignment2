72fa191c3de8b97403acb33e0be2933b94a5e5eb
Huikai Ling <s3738959@student.rmit.edu.au>
2022-10-13 01:46:07 +1100

    update

--- express-api-a2/src/database/config 2.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOST: "rmit.australiaeast.cloudapp.azure.com",
-    USER: "s3789585",
-    PASSWORD: "ctYfnEZsg4mmFHcjjW%va2Q@FZc#@ERce!2^xX&2",
-    DB: "s3789585",
-    DIALECT: "mysql"
-};
